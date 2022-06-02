24270791be82140645b21b3ec80da60e891ae974
Gesiel Neves <neves.gesiel@gmail.com>
2022-06-02 23:38:43 +0044

    Create AdocaoRepository

---
+++ src/main/java/com/gijosc/adoteumpet/core/repositories/AdocaoRepositories.java
@@ -0,0 +1,9 @@
+package com.gijosc.adoteumpet.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gijosc.adoteumpet.core.models.Adocao;
+
+public interface AdocaoRepositories extends JpaRepository<Adocao, Long> {
+  
+}

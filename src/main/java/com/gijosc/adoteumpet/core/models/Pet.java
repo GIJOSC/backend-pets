79fe475e05a3fd338d392ab63300838cce966439
Gesiel Neves <neves.gesiel@gmail.com>
2022-06-02 22:05:09 +0044

    Include Anotations

--- src/main/java/com/gijosc/adoteumpet/core/models/Pet.java
+++ src/main/java/com/gijosc/adoteumpet/core/models/Pet.java
@@ -1,6 +1,10 @@
 package com.gijosc.adoteumpet.core.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,15 +18,18 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(onlyExplicitlyIncluded =  true)
 public class Pet {
 
+ @Id
  @EqualsAndHashCode.Include
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false)
   private String nome;
 
+  @Column(nullable = false)
   private String historia;
 
+  @Column(nullable = false)
   private String foto;
-
-
 
 }
